688df71216b50dfa965ec86f2a166f696e8b43a2
shedivesin <>
2025-08-24 18:06:23 -0600

    much cleaner code

--- generate.mjs
+++ generate.mjs
@@ -1,6 +1,49 @@
+const w = 432;
+const h = 432;
+const r = 215.5;
+const p = 18;
+
+const e = 23.435;
+const cos_e = Math.cos(e * Math.PI / 180);
+const sin_e = Math.sin(e * Math.PI / 180);
+function ecliptic_to_equatorial(lon, lat) {
+  const cos_lon = Math.cos(lon * Math.PI / 180);
+  const sin_lon = Math.sin(lon * Math.PI / 180);
+  const cos_lat = Math.cos(lat * Math.PI / 180);
+  const sin_lat = Math.sin(lat * Math.PI / 180);
+  const tan_lat = sin_lat / cos_lat;
+  return [
+    Math.atan2(sin_lon * cos_e - tan_lat * sin_e, cos_lon) * 12 / Math.PI,
+    Math.asin(sin_lat * cos_e + cos_lat * sin_e * sin_lon) * 180 / Math.PI,
+  ];
+}
+
 const lat = 38.87;
 const cos_lat = Math.cos(lat * Math.PI / 180);
 const sin_lat = Math.sin(lat * Math.PI / 180);
+// NB: Local sidereal time is arbitrary and controls the plate's rotation.
+const lst = 7.65;
+function horizontal_to_equatorial(azi, alt) {
+  const cos_azi = Math.cos(azi * Math.PI / 180);
+  const sin_azi = Math.sin(azi * Math.PI / 180);
+  const cos_alt = Math.cos(alt * Math.PI / 180);
+  const sin_alt = Math.sin(alt * Math.PI / 180);
+  const tan_alt = sin_alt / cos_alt;
+  return [
+    lst - Math.atan2(sin_azi, cos_azi * sin_lat + tan_alt * cos_lat) * 12 / Math.PI,
+    Math.asin(sin_lat * sin_alt - cos_lat * cos_alt * cos_azi) * 180 / Math.PI,
+  ];
+}
+
+function equatorial_to_cartesian(ra, dec) {
+  // Polar azimuthal.
+  // FIXME: Try stereographic?
+  return [
+    w / 2 + Math.cos(ra * Math.PI / 12) * (r - p) * (90 - dec) / (180 - lat),
+    h / 2 + Math.sin(ra * Math.PI / 12) * (r - p) * (90 - dec) / (180 - lat),
+  ];
+}
+
 
 import fs from "node:fs/promises";
 const STARS = (await fs.readFile("stars.csv", "utf8")).
@@ -24,82 +67,67 @@ const LABELS = (await fs.readFile("labels.csv", "utf8")).
     x[4] = +x[4];
     return x;
   });
-const CONSTELLATIONS = JSON.parse(await fs.readFile("constellations.lines.min.geojson", "utf8"));
 
-const e = 23.435;
-const cos_e = Math.cos(e * Math.PI / 180);
-const sin_e = Math.sin(e * Math.PI / 180);
 
-const w = 432;
-const h = 432;
-const r = 215.5;
 console.log("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 %d %d\">", w, h);
 
 // Outer circle.
 console.log("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"white\" stroke=\"black\"/>", w / 2, h / 2, r);
+console.log("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"white\" stroke=\"black\"/>", w / 2, h / 2, r - p);
 
 // Celestial equator.
-// console.log("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"none\" stroke=\"black\" stroke-width=\"0.5\"/>", w / 2, h / 2, r * (90 - 0) / (180 - lat));
+// console.log("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"none\" stroke=\"black\" stroke-width=\"0.5\"/>", w / 2, h / 2, (r - p) * (90 - 0) / (180 - lat));
 
-// Ecliptic markers.
+// Path of the ecliptic.
 {
   let d = "M";
 
-  for(let l = 0; l < 360; l++) {
-    const cos_l = Math.cos(l * Math.PI / 180);
-    const sin_l = Math.sin(l * Math.PI / 180);
-    const ra = Math.atan2(cos_e * sin_l, cos_l) * 12 / Math.PI;
-    const dec = Math.asin(sin_e * sin_l) * 180 / Math.PI;
-    const x = w / 2 + Math.cos(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
-    const y = h / 2 + Math.sin(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
+  for(let lon = 0; lon < 360; lon++) {
+    const [ra, dec] = ecliptic_to_equatorial(lon, 0);
+    const [x, y] = equatorial_to_cartesian(ra, dec);
     d += " " + x + " " + y;
   }
+
+  d += "Z";
 
   console.log("<path d=\"%s\" fill=\"none\" stroke=\"black\" stroke-width=\"0.5\"/>", d);
 }
 
+// Ecliptic markers.
+for(let lon = 0; lon < 360; lon++) {
+  const [ra, dec] = ecliptic_to_equatorial(lon, 0);
+  const len = (lon % 30 === 0)? 1: (lon % 10 === 0)? 0.5: (lon % 5 === 0)? 0.25: 0.125;
+  console.log(
+    "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" stroke-width=\"0.5\"/>",
+    w / 2 + Math.cos(ra * Math.PI / 12) * (r - p * len),
+    h / 2 + Math.sin(ra * Math.PI / 12) * (r - p * len),
+    w / 2 + Math.cos(ra * Math.PI / 12) * r,
+    h / 2 + Math.sin(ra * Math.PI / 12) * r,
+  );
+}
+
+// Stars
 for(const [mag, ra, dec] of STARS) {
   if(dec < lat - 90) { continue; }
 
-  const x = w / 2 + Math.cos(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
-  const y = h / 2 + Math.sin(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
+  const [x, y] = equatorial_to_cartesian(ra, dec);
   const s = 6 * Math.pow(100, (STARS[0][0] - mag) / 10);
 
   console.log("<circle cx=\"%d\" cy=\"%d\" r=\"%d\"/>", x, y, s);
 }
 
 for(const [name, ra, dec, u, v] of LABELS) {
+  const [x, y] = equatorial_to_cartesian(ra, dec);
   console.log(
     "<text transform=\"translate(%d, %d) rotate(%d) translate(%d, %d)\" text-anchor=\"middle\" dominant-baseline=\"middle\" font-family=\"Helvetica Neue\" font-size=\"8\" font-weight=\"300\">%s</text>",
-    w / 2 + Math.cos(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat),
-    h / 2 + Math.sin(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat),
+    x,
+    y,
     ra * 15 - 90,
     u,
     v,
     name,
   );
 }
-
-/*
-for(const {geometry: {coordinates: lines}} of CONSTELLATIONS.features) {
-  let d = "";
-
-  for(const line of lines) {
-    d += "M";
-    for(const [ra, dec] of line) {
-      const x = w / 2 + Math.cos(ra * Math.PI / 180) * r * (90 - dec) / (180 - lat);
-      const y = h / 2 + Math.sin(ra * Math.PI / 180) * r * (90 - dec) / (180 - lat);
-
-      d += " " + x + " " + y;
-    }
-  }
-
-  console.log("<path d=\"%s\" fill=\"none\" stroke=\"black\" stroke-width=\"0.25\"/>", d);
-}
-*/
-
-// NB: Local sidereal time is arbitrary and controls the plate's rotation.
-const lst = 7.65;
 
 {
   let d = "M";
@@ -112,8 +140,8 @@ const lst = 7.65;
     const sin_az = Math.sin(az * Math.PI / 180);
     const ra = lst - Math.atan2(sin_az, cos_az * sin_lat + tan_al * cos_lat) * 12 / Math.PI;
     const dec = Math.asin(sin_lat * sin_al - cos_lat * cos_al * cos_az) * 180 / Math.PI;
-    const x = w / 2 + Math.cos(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
-    const y = h / 2 + Math.sin(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
+    const x = w / 2 + Math.cos(ra * Math.PI / 12) * (r - p) * (90 - dec) / (180 - lat);
+    const y = h / 2 + Math.sin(ra * Math.PI / 12) * (r - p) * (90 - dec) / (180 - lat);
     d += " " + x + " " + y;
   }
 
@@ -121,20 +149,12 @@ const lst = 7.65;
   console.log("<path d=\"%s\" fill=\"none\" stroke=\"red\"/>", d);
 }
 
-for(let az = 90; az < 360; az += 180) {
-  const cos_az = Math.cos(az * Math.PI / 180);
-  const sin_az = Math.sin(az * Math.PI / 180);
+for(let azi = 90; azi < 360; azi += 180) {
   let d = "M";
 
-  for(let al = 0; al <= 90; al++) {
-    const cos_al = Math.cos(al * Math.PI / 180);
-    const sin_al = Math.sin(al * Math.PI / 180);
-    const tan_al = sin_al / cos_al;
-    const ra = lst - Math.atan2(sin_az, cos_az * sin_lat + tan_al * cos_lat) * 12 / Math.PI;
-    const dec = Math.asin(sin_lat * sin_al - cos_lat * cos_al * cos_az) * 180 / Math.PI;
-
-    const x = w / 2 + Math.cos(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
-    const y = h / 2 + Math.sin(ra * Math.PI / 12) * r * (90 - dec) / (180 - lat);
+  for(let alt = 0; alt <= 90; alt++) {
+    const [ra, dec] = horizontal_to_equatorial(azi, alt);
+    const [x, y] = equatorial_to_cartesian(ra, dec);
     d += " " + x + " " + y;
   }
 
