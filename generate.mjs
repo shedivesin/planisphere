f99f69a99f02e1e821ad73917fd44d8620d06308
shedivesin <>
2025-08-25 13:06:08 -0600

    latest

--- generate.mjs
+++ generate.mjs
@@ -3,7 +3,8 @@ const h = 432;
 const r = 215.5;
 const p = 18;
 
-const e = 23.435;
+// https://aas.aanda.org/articles/aas/full/1998/01/ds1449/node3.html
+const e = 23.435; // For equinox 2030.
 const cos_e = Math.cos(e * Math.PI / 180);
 const sin_e = Math.sin(e * Math.PI / 180);
 function ecliptic_to_equatorial(lon, lat) {
@@ -11,10 +12,9 @@ function ecliptic_to_equatorial(lon, lat) {
   const sin_lon = Math.sin(lon * Math.PI / 180);
   const cos_lat = Math.cos(lat * Math.PI / 180);
   const sin_lat = Math.sin(lat * Math.PI / 180);
-  const tan_lat = sin_lat / cos_lat;
   return [
-    Math.atan2(sin_lon * cos_e - tan_lat * sin_e, cos_lon) * 12 / Math.PI,
-    Math.asin(sin_lat * cos_e + cos_lat * sin_e * sin_lon) * 180 / Math.PI,
+    Math.atan2(cos_lat * cos_e * sin_lon - sin_lat * sin_e, cos_lon * cos_lat) * 12 / Math.PI,
+    Math.asin(cos_lat * sin_e * sin_lon + sin_lat * cos_e) * 180 / Math.PI,
   ];
 }
 
@@ -22,7 +22,7 @@ const lat = 38.87;
 const cos_lat = Math.cos(lat * Math.PI / 180);
 const sin_lat = Math.sin(lat * Math.PI / 180);
 // NB: Local sidereal time is arbitrary and controls the plate's rotation.
-const lst = 7.65;
+const lst = 7.655;
 function horizontal_to_equatorial(azi, alt) {
   const cos_azi = Math.cos(azi * Math.PI / 180);
   const sin_azi = Math.sin(azi * Math.PI / 180);
@@ -38,8 +38,8 @@ function horizontal_to_equatorial(azi, alt) {
 function equatorial_to_cartesian(ra, dec) {
   // Polar azimuthal.
   return [
-    w / 2 + Math.cos(ra * Math.PI / 12) * (r - p) * (90 - dec) / (180 - lat),
-    h / 2 + Math.sin(ra * Math.PI / 12) * (r - p) * (90 - dec) / (180 - lat),
+    w / 2 + Math.cos(ra * Math.PI / 12) * (r - p * 2) * (90 - dec) / (180 - lat),
+    h / 2 + Math.sin(ra * Math.PI / 12) * (r - p * 2) * (90 - dec) / (180 - lat),
   ];
 }
 
@@ -92,38 +92,35 @@ console.log("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"white\" stroke=\"black\
   console.log("<path d=\"%s\" fill=\"none\" stroke=\"black\" stroke-width=\"0.5\"/>", d);
 }
 
-// Gates
-for(const lon of [90.02, 270.02]) {
-  const [ra, dec] = ecliptic_to_equatorial(lon, 0);
-  const [x, y] = equatorial_to_cartesian(ra, dec);
-
-  console.log(
-    "<circle cx=\"%d\" cy=\"%d\" r=\"1.5\" fill=\"white\" stroke=\"black\" stroke-width=\"0.5\"/>",
-    x,
-    y,
-  );
-}
-
 // Ecliptic markers.
 for(let lon = 0; lon < 360; lon++) {
   const [ra, dec] = ecliptic_to_equatorial(lon, 0);
-  const len = (lon % 30 === 0)? 1: (lon % 10 === 0)? 0.5: (lon % 5 === 0)? 0.25: 0.125;
+  const len = (lon % 30 === 0)? 5/18: (lon % 10 === 0)? 1/2: (lon % 5 === 0)? 1/4: 1/8;
   console.log(
     "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" stroke-width=\"0.5\"/>",
-    w / 2 + Math.cos(ra * Math.PI / 12) * (r - p * len),
-    h / 2 + Math.sin(ra * Math.PI / 12) * (r - p * len),
-    w / 2 + Math.cos(ra * Math.PI / 12) * r,
-    h / 2 + Math.sin(ra * Math.PI / 12) * r,
+    w / 2 + Math.cos(ra * Math.PI / 12) * (r - p),
+    h / 2 + Math.sin(ra * Math.PI / 12) * (r - p),
+    w / 2 + Math.cos(ra * Math.PI / 12) * (r - p * (1 - len)),
+    h / 2 + Math.sin(ra * Math.PI / 12) * (r - p * (1 - len)),
   );
+
+  if(lon % 30 === 0) {
+    console.log(
+      "<text transform=\"translate(%d, %d) rotate(%d) translate(0, -3)\" text-anchor=\"middle\" font-family=\"Helvetica Neue\" font-size=\"12\" font-weight=\"300\">%s</text>",
+      w / 2 + Math.cos(ra * Math.PI / 12) * r,
+      h / 2 + Math.sin(ra * Math.PI / 12) * r,
+      lon - 90,
+      String.fromCharCode(9800 + Math.floor(lon / 30), 65038),
+    );
+  }
 }
 
 // Stars
 for(const [mag, ra, dec] of STARS) {
-  if(dec < lat - 90) { continue; }
-
   const [x, y] = equatorial_to_cartesian(ra, dec);
-  const s = 6 * Math.pow(100, (STARS[0][0] - mag) / 10);
+  if(Math.hypot(w / 2 - x, h / 2 - y) >= r - p) { continue; }
 
+  const s = 6 * Math.pow(100, (STARS[0][0] - mag) / 10);
   console.log("<circle cx=\"%d\" cy=\"%d\" r=\"%d\"/>", x, y, s);
 }
 
@@ -141,6 +138,37 @@ for(const [name, ra, dec, u, v] of LABELS) {
   );
 }
 
+// Upper plate edge.
+console.log(
+  "<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"none\" stroke=\"red\"/>",
+  w / 2,
+  h / 2,
+  r - p,
+);
+
+// Upper plate markers.
+for(let min = 0; min < 1440; min += 5) {
+  const len = (min % 60 === 0)? 9/18: (min % 30 === 0)? 0.5: (min % 15 === 0)? 0.25: 0.125;
+  console.log(
+    "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"red\" stroke-width=\"0.5\"/>",
+    w / 2 + Math.cos((lst * 60 + min) * Math.PI / 720) * (r - p * (1 + len)),
+    h / 2 + Math.sin((lst * 60 + min) * Math.PI / 720) * (r - p * (1 + len)),
+    w / 2 + Math.cos((lst * 60 + min) * Math.PI / 720) * (r - p),
+    h / 2 + Math.sin((lst * 60 + min) * Math.PI / 720) * (r - p),
+  );
+
+  if(min % 60 === 0) {
+    console.log(
+      "<text transform=\"translate(%d, %d) rotate(%d) translate(0, 1)\" text-anchor=\"middle\" dominant-baseline=\"hanging\" font-family=\"Helvetica Neue\" font-size=\"9\" font-weight=\"300\" fill=\"red\">%s</text>",
+      w / 2 + Math.cos((lst * 60 + min) * Math.PI / 720) * (r - p * 2),
+      h / 2 + Math.sin((lst * 60 + min) * Math.PI / 720) * (r - p * 2),
+      (lst * 60 + min) * 180 / 720 - 90,
+      (36 - Math.floor(min / 60)) % 24,
+    );
+  }
+}
+
+// Upper plate horizon.
 {
   let d = "M";
 
@@ -154,24 +182,19 @@ for(const [name, ra, dec, u, v] of LABELS) {
   console.log("<path d=\"%s\" fill=\"none\" stroke=\"red\"/>", d);
 }
 
-for(let azi = 90; azi < 360; azi += 180) {
-  let d = "M";
-
-  for(let alt = 0; alt <= 90; alt++) {
-    const [ra, dec] = horizontal_to_equatorial(azi, alt);
-    const [x, y] = equatorial_to_cartesian(ra, dec);
-    d += " " + x + " " + y;
-  }
-
-  console.log("<path d=\"%s\" fill=\"none\" stroke=\"red\" stroke-width=\"0.5\"/>", d);
+// Upper plate meridian.
+{
+  const [ra1, dec1] = horizontal_to_equatorial(0, 0);
+  const [ra2, dec2] = horizontal_to_equatorial(180, 0);
+  const [x1, y1] = equatorial_to_cartesian(ra1, dec1);
+  const [x2, y2] = equatorial_to_cartesian(ra2, dec2);
+  console.log(
+    "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"red\" stroke-width=\"0.5\"/>",
+    x1,
+    y1,
+    x2,
+    y2,
+  );
 }
-
-console.log(
-  "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"red\" stroke-width=\"0.5\"/>",
-  w / 2 + Math.cos((lst * 15) * Math.PI / 180) * r,
-  h / 2 + Math.sin((lst * 15) * Math.PI / 180) * r,
-  w / 2 + Math.cos((lst * 15 + 180) * Math.PI / 180) * r,
-  h / 2 + Math.sin((lst * 15 + 180) * Math.PI / 180) * r,
-);
 
 console.log("</svg>");
